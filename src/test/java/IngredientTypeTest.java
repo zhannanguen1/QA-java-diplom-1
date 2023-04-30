aa128056b969d02947df092e86e22e262222a7a7
Zhanna Nguen <zhanna.nguen@mail.ru>
2023-04-30 21:35:01 +0300

    добавила параметризированные тесты для класса IngredientType

--- src/test/java/IngredientTypeTest.java
+++ src/test/java/IngredientTypeTest.java
@@ -1,2 +1,15 @@
+import org.junit.Test;
+import praktikum.IngredientType;
+import static org.junit.Assert.assertNotNull;
+
 public class IngredientTypeTest {
+    @Test
+    public void testSauceIsNotNull() {
+        assertNotNull(IngredientType.valueOf("SAUСE"));
+    }
+
+    @Test
+    public void testFillingIsNotNull() {
+        assertNotNull(IngredientType.valueOf("FILLING"));
+    }
 }
